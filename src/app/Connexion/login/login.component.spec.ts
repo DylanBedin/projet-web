40769a365396bb4c83bef2522c2353a1f893755e
Azalkor <jeremysabourin24@gmail.com>
2017-12-06 00:26:44 +0100

    ajout du component d'authentification, ajout bouton add livre, debug book-create (route)

---
+++ src/app/Connexion/login/login.component.spec.ts
@@ -0,0 +1,29 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {AuthenticationService} from '../../services/authentication-service';
+import { HttpClientModule } from '@angular/common/http';
+
+
+import { Router, ActivatedRoute } from '@angular/router';
+import { LoginComponent } from './login.component';
+
+describe('LoginComponent', () => {
+  let component: LoginComponent;
+  let fixture: ComponentFixture<LoginComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ LoginComponent ]
+    })
+    .compileComponents();
+  }));
+5
+  beforeEach(() => {
+    fixture = TestBed.createComponent(LoginComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
