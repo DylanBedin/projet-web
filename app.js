870b47c5982e78e64e1f62736ca388bfd9fec598
AntBorde <antoine33.borde@gmail.com>
2017-12-17 21:37:03 +0100

    activation de cors + logger

--- app.js
+++ app.js
@@ -3,6 +3,15 @@ var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var bodyParser = require('body-parser');
+var cors = require('cors');
+
+var originsWhitelist = [
+    'http://localhost:4200',
+    'http://localhost:3000',
+    'http://localhost'
+];
+
+
 
 //Differents types of works
 var album = require('./server/routes/album');
@@ -11,6 +20,7 @@ var game = require('./server/routes/game');
 var movie = require('./server/routes/movie');
 var serie = require('./server/routes/serie');
 var user = require('./server/routes/user');
+
 
 var mongoose = require('mongoose');
 mongoose.Promise = require('bluebird');
@@ -22,10 +32,19 @@ mongoose.connect('mongodb://userweb2017:pwdweb2017@ds059546.mlab.com:59546/dbweb
 var distDir = __dirname + "/dist/";
 
 var app = express();
+app.use(logger('dev'));
 
 app.use(express.static(distDir));
 // Initialise l'app.
 app.use(bodyParser.json())
+var corsOptions = {
+    origin: function(origin, callback){
+        var isWhitelisted = originsWhitelist.indexOf(origin) !== -1;
+        callback(null, isWhitelisted);
+    },
+    credentials:true
+}
+app.use(cors(corsOptions));
 
 app.use('/albums', album);
 app.use('/books', book);
@@ -34,6 +53,8 @@ app.use('/movies', movie);
 app.use('/series', serie);
 
 app.use('/users',user);
+
+
 
 
 app.get('*', (req, res) => {
