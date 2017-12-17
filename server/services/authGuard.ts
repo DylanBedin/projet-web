e783a59adbfd445948a5b7e0d86c62fd5e2de55d
dylanbed <dylan.bedin@gmail.com>
2017-12-17 23:18:40 +0100

    ajout gardes routes

---
+++ server/services/authGuard.ts
@@ -0,0 +1,20 @@
+import {ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot} from '@angular/router';
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class AuthGuardDisconnected implements CanActivate {
+    constructor(private router: Router) {}
+
+    canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+        return sessionStorage.getItem('userID') == null;
+    }
+}
+
+@Injectable()
+export class AuthGuardConnected implements CanActivate {
+    constructor(private router: Router) {}
+
+    canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+        return sessionStorage.getItem('userID') != null;
+    }
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -46,168 +46,215 @@ import { MovieCollectionComponent } from './Movie/movie-collection/movie-collect
 import { MovieWishlistComponent } from './Movie/movie-wishlist/movie-wishlist.component';
 import { SerieCollectionComponent } from './Serie/serie-collection/serie-collection.component';
 import { SerieWishlistComponent } from './Serie/serie-wishlist/serie-wishlist.component';
-
+import {AuthGuardConnected, AuthGuardDisconnected} from "../../server/services/authGuard";
 
 const appRoutes: Routes = [
-    { path: 'login', component: LoginComponent },
-    { path: 'register', component: RegisterComponent },
-    { path: 'parcourir', component: ParcourirComponent},
-    { path: 'collection', component: CollectionComponent},
-    { path: 'envies', component: EnviesComponent},
+    { path: 'login', component: LoginComponent, canActivate: [AuthGuardDisconnected] },
+    { path: 'register', component: RegisterComponent, canActivate: [AuthGuardDisconnected]},
+    
+    
+    { path: 'parcourir', component: ParcourirComponent, canActivate: [AuthGuardConnected]},
+    { path: 'collection', component: CollectionComponent, canActivate: [AuthGuardConnected]},
+    { path: 'envies', component: EnviesComponent, canActivate: [AuthGuardConnected]},
     {
         path: 'parcourir/albums',
         component: AlbumComponent,
-        data: {title: 'Album List'}
+        data: {title: 'Album List'},
+        canActivate: [AuthGuardConnected]
     },
     {
         path: 'collection/albums',
         component: AlbumCollectionComponent,
-        data: {title: 'Album Collection'}
+        data: {title: 'Album Collection'},
+        canActivate: [AuthGuardConnected]
     },
     {
         path: 'envies/albums',
         component: AlbumWishlistComponent,
-        data: {title: 'Album List'}
+        data: {title: 'Album List'},
+        canActivate: [AuthGuardConnected]
     },
     {
         path: 'album-detail/:id',
         component: AlbumDetailComponent,
-        data: {title: 'Album Details'}
+        data: {title: 'Album Details'},
+        canActivate: [AuthGuardConnected]
     },
     {
         path: 'album-create',
         component: AlbumCreateComponent,
-        data: {title: 'Create Album'}
+        data: {title: 'Create Album'},
+        canActivate: [AuthGuardConnected]
     },
     {
         path: 'album-edit/:id',
         component: AlbumEditComponent,
-        data: {title: 'Edit Album'}
+        data: {title: 'Edit Album'},
+        canActivate: [AuthGuardConnected]
     },
     {
         path: 'parcourir/books',
         component: BookComponent,
-        data: {title: 'Book List'}
+        data: {title: 'Book List'},
+        canActivate: [AuthGuardConnected]
     },
     {
         path: 'collection/books',
         component: BookCollectionComponent,
-        data: {title: 'Book Collection'}
+        data: {title: 'Book Collection'},
+        canActivate: [AuthGuardConnected]
     },
     {
         path: 'envies/books',
         component: BookWishlistComponent,
-        data: {title: 'Book Wishlist'}
+        data: {title: 'Book Wishlist'},
+        canActivate: [AuthGuardConnected]
     },
     {
         path: 'book-detail/:id',
         component: BookDetailComponent,
-        data: {title: 'Book Details'}
+        data: {title: 'Book Details'},
+        canActivate: [AuthGuardConnected]
     },
     {
         path: 'book-create',
         component: BookCreateComponent,
-        data: {title: 'Create Book'}
+        data: {title: 'Create Book'},
+        canActivate: [AuthGuardConnected]
     },
     {
         path: 'book-edit/:id',
         component: BookEditComponent,
-        data: {title: 'Edit Book'}
+        data: {title: 'Edit Book'},
+        canActivate: [AuthGuardConnected]
     },
     {
         path: 'parcourir/games',
         component: GameComponent,
-        data: {title: 'Game List'}
+        data: {title: 'Game List'},
+        canActivate: [AuthGuardConnected]
     },
     {
         path: 'collection/games',
         component: GameCollectionComponent,
-        data: {title: 'Game Collection'}
+        data: {title: 'Game Collection'},
+        canActivate: [AuthGuardConnected]
     },
     {
         path: 'envies/games',
         component: GameWishlistComponent,
-        data: {title: 'Games Wishlist'}
+        data: {title: 'Games Wishlist'},
+        canActivate: [AuthGuardConnected]
     },
     {
         path: 'game-detail/:id',
         component: GameDetailComponent,
-        data: {title: 'Game Details'}
+        data: {title: 'Game Details'},
+        canActivate: [AuthGuardConnected]
     },
     {
         path: 'game-create',
         component: GameCreateComponent,
-        data: {title: 'Create Game'}
+        data: {title: 'Create Game'},
+        canActivate: [AuthGuardConnected]
     },
     {
         path: 'game-edit/:id',
         component: GameEditComponent,
-        data: {title: 'Edit Game'}
+        data: {title: 'Edit Game'},
+        canActivate: [AuthGuardConnected]
     },
     {
         path: 'parcourir/movies',
         component: MovieComponent,
-        data: {title: 'Movie List'}
+        data: {title: 'Movie List'},
+        canActivate: [AuthGuardConnected]
     },
     {
         path: 'collection/movies',
         component: MovieCollectionComponent,
-        data: {title: 'Movie Collection'}
+        data: {title: 'Movie Collection'},
+        canActivate: [AuthGuardConnected]
     },
     {
         path: 'envies/movies',
         component: MovieWishlistComponent,
-        data: {title: 'Movie Wishlist'}
+        data: {title: 'Movie Wishlist'},
+        canActivate: [AuthGuardConnected]
     },
     {
         path: 'movie-detail/:id',
         component: MovieDetailComponent,
-        data: {title: 'Movie Details'}
+        data: {title: 'Movie Details'},
+        canActivate: [AuthGuardConnected]
     },
     {
         path: 'movie-create',
         component: MovieCreateComponent,
-        data: {title: 'Create Movie'}
+        data: {title: 'Create Movie'},
+        canActivate: [AuthGuardConnected]
     },
     {
         path: 'movie-edit/:id',
         component: MovieEditComponent,
-        data: {title: 'Edit Movie'}
+        data: {title: 'Edit Movie'},
+        canActivate: [AuthGuardConnected]
     },
     {
         path: 'parcourir/series',
         component: SerieComponent,
-        data: {title: 'Serie List'}
+        data: {title: 'Serie List'},
+        canActivate: [AuthGuardConnected]
     },
     {
         path: 'collection/series',
         component: SerieCollectionComponent,
-        data: {title: 'Serie List'}
+        data: {title: 'Serie List'},
+        canActivate: [AuthGuardConnected]
     },
     {
         path: 'envies/series',
         component: SerieWishlistComponent,
-        data: {title: 'Serie List'}
+        data: {title: 'Serie List'},
+        canActivate: [AuthGuardConnected]
     },
     {
         path: 'serie-detail/:id',
         component: SerieDetailComponent,
-        data: {title: 'Serie Details'}
+        data: {title: 'Serie Details'},
+        canActivate: [AuthGuardConnected]
     },
     {
         path: 'serie-create',
         component: SerieCreateComponent,
-        data: {title: 'Create Serie'}
+        data: {title: 'Create Serie'},
+        canActivate: [AuthGuardConnected]
     },
     {
         path: 'serie-edit/:id',
         component: SerieEditComponent,
-        data: {title: 'Edit Serie'}
+        data: {title: 'Edit Serie'},
+        canActivate: [AuthGuardConnected]
+    },
+    {
+        path: '**',
+        redirectTo: 'login',
+        pathMatch: 'full',
+    },
+    {
+        path: '**',
+        redirectTo: 'parcourir',
+        pathMatch: 'full',
     },
     {
         path: '',
         redirectTo: 'login',
-        pathMatch: 'full'
+        pathMatch: 'full',
+    },
+    {
+        path: '',
+        redirectTo: 'parcourir',
+        pathMatch: 'full',
     }
 ];
 
@@ -266,6 +313,8 @@ const appRoutes: Routes = [
     ],
     providers: [
         AuthenticationService,
+        AuthGuardConnected,
+        AuthGuardDisconnected,
         UserService,
     ],
     bootstrap: [AppComponent]
