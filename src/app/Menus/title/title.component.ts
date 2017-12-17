15f0d4ecc2d2498dc74c660967216c41a54a92f5
Azalkor <jeremysabourin24@gmail.com>
2017-12-17 17:06:45 +0100

    app-menu devient app-title

--- src/app/Menus/title/title.component.ts
+++ src/app/Menus/title/title.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 
 @Component({
-    selector: 'app-menu',
+    selector: 'app-title',
     templateUrl: './title.component.html',
     styleUrls: ['./title.component.css']
 })
@@ -14,7 +14,9 @@ export class TitleComponent implements OnInit {
 
     ngOnInit() {
         this.http.get('/users/' + sessionStorage.getItem('userID')).subscribe(user => {
+
             this.currentUser = user;
+            console.log(this.currentUser);
         });
     }
 }
