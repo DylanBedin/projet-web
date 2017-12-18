3330cdef700540f39bc0501ec99de3f81440722e
Azalkor <jeremysabourin24@gmail.com>
2017-12-18 23:36:30 +0100

    changement des models, mise au propre de book

--- server/models/Album.js
+++ server/models/Album.js
@@ -3,10 +3,7 @@ var mongoose = require('mongoose');
 var AlbumSchema = new mongoose.Schema({
     title: String,
     author: String,
-    description: String,
     published_year: String,
-    publisher: String,
-    genre: String,
     avis: [String],
     note: {type: Number, default: 0},
     nbVotants: {type: Number, default: 0},
--- server/models/Game.js
+++ server/models/Game.js
@@ -2,14 +2,9 @@ var mongoose = require('mongoose');
 
 var GameSchema = new mongoose.Schema({
     title: String,
-    author: String,
-    description: String,
     published_year: String,
     developer : String,
     publisher: String,
-    producer: String,
-    genre: String,
-    platforms: [String],
     avis: [String],
     note: {type: Number, default: 0},
     nbVotants: {type: Number, default: 0},
