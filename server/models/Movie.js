18c3a0cbb578b03180db82db96fa2f4912f7a2d9
dylanbed <dylan.bedin@gmail.com>
2017-12-18 15:40:37 +0100

    note+avis movie

--- server/models/Movie.js
+++ server/models/Movie.js
@@ -9,6 +9,10 @@ var MovieSchema = new mongoose.Schema({
     description: String,
     published_year: String,
     genre: String,
+    avis: [String],
+    note: {type: Number, default: 0},
+    nbVotants: {type: Number, default: 0},
+    hasNoted: [String],
     updated_date: {type: Date, default: Date.now},
 });
 
